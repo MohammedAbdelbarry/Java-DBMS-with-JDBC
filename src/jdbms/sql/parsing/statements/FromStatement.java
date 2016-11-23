0cc1ba7fa5bfcf0e912ea6733e552d853c2c847f
Mohammed Abdelbarry <m7md337@gmail.com>
2016-11-23 11:22:41 +0200

    modified from statement

--- src/jdbms/sql/parsing/statements/FromStatement.java
+++ src/jdbms/sql/parsing/statements/FromStatement.java
@@ -1,10 +1,12 @@
 package jdbms.sql.parsing.statements;
 
+import jdbms.sql.parsing.expressions.Expression;
+
 public class FromStatement extends Statement {
 	private static final String STATEMENT_IDENTIFIER = "FROM";
 
-	public FromStatement() {
-		super(null, STATEMENT_IDENTIFIER);
+	public FromStatement(Expression nextExpression) {
+		super(nextExpression, STATEMENT_IDENTIFIER);
 	}
 
 }
