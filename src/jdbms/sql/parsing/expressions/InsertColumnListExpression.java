fed644a09e6455a6a9169437785cdb05f26546b2
Ahmed Walid <ahmed3walid96@gmail.com>
2016-11-23 22:52:53 +0200

    Added some insert expressions

---
+++ src/jdbms/sql/parsing/expressions/InsertColumnListExpression.java
@@ -0,0 +1,30 @@
+package jdbms.sql.parsing.expressions;
+
+import java.util.List;
+
+import jdbms.sql.parsing.expressions.util.ColumnExpression;
+
+public class InsertColumnListExpression extends ColumnListExpression {
+
+	List<ColumnExpression> columnsNames;
+	public InsertColumnListExpression() {
+		super(new ValueListExpression());
+	}
+
+	@Override
+	public boolean interpret(String sqlExpression) {
+		String[] columns = sqlExpression.split(",");
+		columns[columns.length - 1] = columns[columns.length - 1].trim();
+		String restOfExp = columns[columns.length - 1]
+				.substring(columns[columns.length - 1].indexOf(" ") + 1);
+		columns[columns.length - 1] = columns[columns.length - 1]
+				.substring(0, columns[columns.length - 1].indexOf(" "));
+		for (int i = 0; i < columns.length; i++) {
+			columnsNames.add(new ColumnExpression(columns[i]));
+			if (!columnsNames.get(i).isValidColumnName()) {
+				return false;
+			}
+		}
+		return super.interpret(restOfExp);
+	}
+}
--- src/jdbms/sql/parsing/expressions/TerminalExpression.java
+++ src/jdbms/sql/parsing/expressions/TerminalExpression.java
@@ -2,6 +2,8 @@ package jdbms.sql.parsing.expressions;
 
 public class TerminalExpression implements Expression {
 
+	public TerminalExpression() {}
+
 	@Override
 	public boolean interpret(String sqlExpression) {
 		if (sqlExpression.equals(";")) {
