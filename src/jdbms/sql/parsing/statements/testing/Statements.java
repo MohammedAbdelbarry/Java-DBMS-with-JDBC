95ce50b41e5a299280375fd9a0653e93fb4804b9
Hisham Ghazy <hisho_96@yahoo.com>
2016-11-25 16:05:17 +0200

    "Added Statements Testing Classs"

---
+++ src/jdbms/sql/parsing/statements/testing/Statements.java
@@ -0,0 +1,249 @@
+package jdbms.sql.parsing.statements.testing;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import jdbms.sql.parsing.parser.Parser;
+import jdbms.sql.parsing.statements.Statement;
+import jdbms.sql.parsing.statements.util.StatementFactory;
+
+public class Statements {
+
+	Collection<Statement> statements = new ArrayList<>();
+	boolean check = false;
+	Parser p = new Parser();
+
+	@Before
+	public void executedBeforeEach() {
+		try {
+			Class.forName("jdbms.sql.parsing.statements.CreateDatabaseStatement");
+			Class.forName("jdbms.sql.parsing.statements.CreateTableStatement");
+			Class.forName("jdbms.sql.parsing.statements.DropDatabaseStatement");
+			Class.forName("jdbms.sql.parsing.statements.DropTableStatement");
+			Class.forName("jdbms.sql.parsing.statements.InsertIntoStatement");
+			Class.forName("jdbms.sql.parsing.statements.DeleteStatement");
+			Class.forName("jdbms.sql.parsing.statements.SelectStatement");
+			Class.forName("jdbms.sql.parsing.statements.UpdateStatement");
+			Class.forName("jdbms.sql.parsing.expressions.math.EqualsExpression");
+			Class.forName("jdbms.sql.parsing.expressions.math.LargerThanEqualsExpression");
+			Class.forName("jdbms.sql.parsing.expressions.math.LessThanEqualsExpression");
+			Class.forName("jdbms.sql.parsing.expressions.math.LargerThanExpression");
+			Class.forName("jdbms.sql.parsing.expressions.math.LessThanExpression");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		for (String key : StatementFactory.getInstance().getRegisteredStatements()) {
+			statements.add(StatementFactory.getInstance().createStatement(key));
+		}
+	}
+
+	@Test
+	public void testCreateDataBase() {
+		String SQLCommand = "CREATE DATABASE my_Database;";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+
+	@Test
+	public void testDropDataBase() {
+		String SQLCommand = "DROP database My_SQLDatabase ;";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+
+	@Test
+	public void testCreateTable() {
+		String SQLCommand = "CREATE table Students(StudentID int,LastName varchar,FirstName varchar,Address varchar,Grades int);";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+
+	@Test
+	public void testDropTable() {
+		String SQLCommand = "drop table My_SQLDatabase ;";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+
+	@Test
+	public void testInsertInto() {
+		String SQLCommand = "INSERT into my_TABLE(A_B,C_D) VALUES (\"x\", 'y'), (z, m);";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+	
+	@Test
+	public void testInsertIntoGluedExpression() {
+		String SQLCommand = "INSERT INTO Customers(CustomerName, ContactName, Address, City, PostalCode, Country)VALUES(12345,'Tom B. Erichsen','Skagen 21','Stavanger',4006,'Norway');";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+	
+	@Test
+	public void testInsertIntoWithoutColumns() {
+		String SQLCommand = "INSERT INTO Customers VALUES(12345,'Tom B. Erichsen','Skagen 21','Stavanger',4006,'Norway');";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+	
+	@Test
+	public void testSelectAll() {
+		String SQLCommand = "SELECT * FROM table_name;";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+
+	@Test
+	public void testSelectAllConditional() {
+		String SQLCommand = "SELECT * FROM Customers WHERE CustomerID>=1;";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+	
+	@Test
+	public void testSelect() {
+		String SQLCommand = "SELECT CustomerID,CustomerName,  Grades FROM Customers;";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+
+	@Test
+	public void testSelectConditionalVarchar() {
+		String SQLCommand = "SELECT CustomerID,CustomerName FROM Customers WHERE Country = 'Germany' ;";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+	
+	@Test
+	public void testSelectConditionalInt() {
+		String SQLCommand = "SELECT CustomerID,CustomerName FROM Customers WHERE id = 7 ;";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+
+	@Test
+	public void testUpdate() {
+		String SQLCommand = "UPDATE Customers SET ContactName='AlfredSchmidt', City='Hamburg' WHERE CustomerName='AlfredsFutterkiste';";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+	
+	@Test
+	public void testUpdateGlued() {
+		String SQLCommand = "UPDATE Customers SET ContactName='Alfred Schmidt',City='Hamburg' WHERE CustomerName='Alfreds Futterkiste';";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+	
+	@Test
+	public void testUpdateAll() {
+		String SQLCommand = "UPDATE CUSTOMERS SET ADDRESS='Pune',SALARY=1000.00;";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+
+	@Test
+	public void testDeleteAll() {
+		String SQLCommand = "DELETE FROM table_name;";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+
+	@Test
+	public void testDeleteConditional() {
+		String SQLCommand = "DELETE FROM Customers WHERE name='xy';";
+		SQLCommand = p.normalizeCommand(SQLCommand);
+		for (Statement statement : statements) {
+			if (statement.interpret(SQLCommand)) {
+				check = true;
+			}
+		}
+		assertEquals(check, true);
+	}
+
+}
