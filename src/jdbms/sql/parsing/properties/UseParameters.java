62182f68f1043878c4ec2ae20025375b0f72e8a7
Mohammed Abdelbarry <m7md337@gmail.com>
2016-11-25 18:07:39 +0200

    added use statement

---
+++ src/jdbms/sql/parsing/properties/UseParameters.java
@@ -0,0 +1,14 @@
+package jdbms.sql.parsing.properties;
+
+public class UseParameters {
+	private String databaseName;
+	public UseParameters() {
+
+	}
+	public String getDatabaseName() {
+		return databaseName;
+	}
+	public void setDatabaseName(String database) {
+		this.databaseName = database;
+	}
+}
