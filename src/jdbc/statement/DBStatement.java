fb99fd1521a0e176988abbd835067d71ea4adfaa
Hisham Ghazy <hisho_96@yahoo.com>
2016-12-11 14:22:58 +0200

    Improvements in ResultSet and Statements

--- src/jdbc/statement/DBStatement.java
+++ src/jdbc/statement/DBStatement.java
@@ -12,24 +12,30 @@ import jdbms.sql.DBMSConnector;
 
 public class DBStatement implements Statement {
 	private boolean isClosed = false;
-	private final Queue<String> commands
-	= new LinkedList<>();
+	private final Queue<String> commands = new LinkedList<>();
 	private final DBMSConnector dbmsConnector;
 
-	public DBStatement(final
-			DBMSConnector connector) {
+	public DBStatement(final DBMSConnector connector) {
 		this.dbmsConnector = connector;
 	}
 
 	@Override
-	public void addBatch(final String sql)
-			throws SQLException {
+	public void addBatch(final String sql) throws SQLException {
+		
+		if (isClosed) {
+			throw new SQLException();
+		}
+		
 		commands.add(sql);
 	}
 
 	@Override
-	public void clearBatch()
-			throws SQLException {
+	public void clearBatch() throws SQLException {
+		
+		if (isClosed) {
+			throw new SQLException();
+		}
+		
 		commands.clear();
 	}
 
@@ -39,8 +45,12 @@ public class DBStatement implements Statement {
 	}
 
 	@Override
-	public boolean execute(final String sql)
-			throws SQLException {
+	public boolean execute(final String sql) throws SQLException {
+		
+		if (isClosed) {
+			throw new SQLException();
+		}
+		
 		if (dbmsConnector.interpretQuery(sql)) {
 			dbmsConnector.executeQuery(sql);
 			return true;
@@ -53,8 +63,7 @@ public class DBStatement implements Statement {
 	}
 
 	@Override
-	public int[] executeBatch()
-			throws SQLException {
+	public int[] executeBatch() throws SQLException {
 		final int[] updateCounts;
 		final int size = commands.size();
 		for (int i = 0; i < size; i++) {
@@ -64,8 +73,7 @@ public class DBStatement implements Statement {
 	}
 
 	@Override
-	public ResultSet executeQuery(final
-			String arg0) throws SQLException {
+	public ResultSet executeQuery(final String arg0) throws SQLException {
 		if (!dbmsConnector.interpretQuery(arg0)) {
 			throw new SQLException();
 		} else {
@@ -75,8 +83,7 @@ public class DBStatement implements Statement {
 	}
 
 	@Override
-	public int executeUpdate(final
-			String sql) throws SQLException {
+	public int executeUpdate(final String sql) throws SQLException {
 		if (!dbmsConnector.interpretUpdate(sql)) {
 			throw new SQLException();
 		} else {
@@ -85,147 +92,112 @@ public class DBStatement implements Statement {
 	}
 
 	@Override
-	public Connection getConnection()
-			throws SQLException {
+	public Connection getConnection() throws SQLException {
 		return null;
 	}
 
 	@Override
-	public ResultSet getResultSet()
-			throws SQLException {
+	public ResultSet getResultSet() throws SQLException {
 		return null;
 	}
 
 	@Override
-	public int getUpdateCount()
-			throws SQLException {
+	public int getUpdateCount() throws SQLException {
 		return 0;
 	}
 
 	@Override
-	public boolean isClosed()
-			throws SQLException {
+	public boolean isClosed() throws SQLException {
 		return isClosed;
 	}
 
 	@Override
-	public boolean isWrapperFor(final
-			Class<?> arg0)
-					throws SQLException {
+	public boolean isWrapperFor(final Class<?> arg0) throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public <T> T unwrap(final
-			Class<T> arg0)
-					throws SQLException {
-		throw new UnsupportedOperationException();
-	}
-
-
-	@Override
-	public void cancel()
-			throws SQLException {
-		throw new UnsupportedOperationException();
-	}
-
-
-	@Override
-	public void clearWarnings()
-			throws SQLException {
-		throw new UnsupportedOperationException();
-	}
-
-
-	@Override
-	public void closeOnCompletion()
-			throws SQLException {
-		throw new UnsupportedOperationException();
-	}
-
-
-	@Override
-	public boolean execute(final String
-			arg0, final int arg1)
-					throws SQLException {
+	public <T> T unwrap(final Class<T> arg0) throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public boolean execute(final String
-			arg0, final int[] arg1)
-					throws SQLException {
+	public void cancel() throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public boolean execute(final String arg0
-			, final String[] arg1)
-					throws SQLException {
+	public void clearWarnings() throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public int executeUpdate(final String
-			arg0, final int arg1)
-					throws SQLException {
+	public void closeOnCompletion() throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public int executeUpdate(final String
-			arg0, final int[] arg1)
-					throws SQLException {
+	public boolean execute(final String arg0, final int arg1) throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public int executeUpdate(final
-			String arg0, final
-			String[] arg1) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
-
-
-	@Override
-	public int getFetchDirection()
-			throws SQLException {
+	public boolean execute(final String arg0, final int[] arg1) throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public int getFetchSize()
-			throws SQLException {
+	public boolean execute(final String arg0, final String[] arg1) throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public ResultSet getGeneratedKeys()
-			throws SQLException {
+	public int executeUpdate(final String arg0, final int arg1) throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public int getMaxFieldSize()
-			throws SQLException {
+	public int executeUpdate(final String arg0, final int[] arg1) throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public int getMaxRows()
-			throws SQLException {
+	public int executeUpdate(final String arg0, final String[] arg1) throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public boolean getMoreResults()
-			throws SQLException {
+	public int getFetchDirection() throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public boolean getMoreResults(final int arg0)
-			throws SQLException {
+	public int getFetchSize() throws SQLException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ResultSet getGeneratedKeys() throws SQLException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int getMaxFieldSize() throws SQLException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int getMaxRows() throws SQLException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean getMoreResults() throws SQLException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean getMoreResults(final int arg0) throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
@@ -234,38 +206,30 @@ public class DBStatement implements Statement {
 		throw new UnsupportedOperationException();
 	}
 
-
 	@Override
-	public int getResultSetConcurrency()
-			throws SQLException {
+	public int getResultSetConcurrency() throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public int getResultSetHoldability()
-			throws SQLException {
+	public int getResultSetHoldability() throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public int getResultSetType()
-			throws SQLException {
-		throw new UnsupportedOperationException();
-	}
-
-
-	@Override
-	public SQLWarning getWarnings()
-			throws SQLException {
+	public int getResultSetType() throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public boolean isCloseOnCompletion()
-			throws SQLException {
+	public SQLWarning getWarnings() throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 
+	@Override
+	public boolean isCloseOnCompletion() throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
 	@Override
 	public boolean isPoolable() throws SQLException {
@@ -273,57 +237,49 @@ public class DBStatement implements Statement {
 	}
 
 	@Override
-	public void setCursorName(final String arg0)
-			throws SQLException {
+	public void setCursorName(final String arg0) throws SQLException {
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
-	public void setEscapeProcessing(final boolean
-			arg0) throws SQLException {
+	public void setEscapeProcessing(final boolean arg0) throws SQLException {
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
-	public void setFetchDirection(final int
-			arg0) throws SQLException {
+	public void setFetchDirection(final int arg0) throws SQLException {
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
-	public void setFetchSize(final int
-			arg0) throws SQLException {
+	public void setFetchSize(final int arg0) throws SQLException {
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
-	public void setMaxFieldSize(final int
-			arg0) throws SQLException {
+	public void setMaxFieldSize(final int arg0) throws SQLException {
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
-	public void setMaxRows(final int
-			arg0) throws SQLException {
+	public void setMaxRows(final int arg0) throws SQLException {
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
-	public void setPoolable(final boolean
-			arg0) throws SQLException {
+	public void setPoolable(final boolean arg0) throws SQLException {
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
-	public void setQueryTimeout(final int
-			arg0) throws SQLException {
+	public void setQueryTimeout(final int arg0) throws SQLException {
 		throw new UnsupportedOperationException();
 	}
 }
