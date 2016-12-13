304b802c9c6eef2e03dd3903768fde3fb7c869fd
Ahmed Walid <ahmed3walid96@gmail.com>
2016-12-13 21:37:53 +0200

    Fixed merge conflicts.

--- src/jdbc/connections/DBConnection.java
+++ src/jdbc/connections/DBConnection.java
@@ -49,7 +49,6 @@ public class DBConnection implements Connection {
 			throw new SQLException();
 		}
 		final DBStatement newStatement = new DBStatement(connector, this);
-
 		statements.add(newStatement);
 		return newStatement;
 	}
